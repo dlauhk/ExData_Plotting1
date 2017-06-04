f1c45bf20405044e30b95e7091c24662a79ecbfc
dlauhk <dicklau21@yahoo.com.hk>
2017-06-04 22:17:12 +0800

    Add files via upload

---
+++ Plot2.R
@@ -0,0 +1,24 @@
+# Course Poject 1
+# Exploratory Data Analysis on Electric Power Consumption
+# Plot 2 - Line Chart on Global Active Power
+
+# Load data
+fileurl <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+download.file(fileurl, destfile = './power.zip')
+unzip('./power.zip', './household_power_consumption.txt')
+power <- read.table('./household_power_consumption.txt', sep=';',header=TRUE)
+power$Date <- as.Date(power$Date, '%d/%m/%Y')
+power2 <- power[ which( power$Date == as.Date('01/02/2007', "%d/%m/%Y") | power$Date == as.Date('02/02/2007', "%d/%m/%Y")), ]
+
+power2$Global_active_power <- as.numeric(power2$Global_active_power)
+power2$Global_reactive_power <- as.numeric(power2$Global_reactive_power)
+power2$Voltage <- as.numeric(power2$Voltage)
+power2$Sub_metering_1 <- as.numeric(power2$Sub_metering_1)
+power2$Sub_metering_2 <- as.numeric(power2$Sub_metering_2)
+power2$Sub_metering_3 <- as.numeric(power2$Sub_metering_3)
+power2$Date2 <- strptime(paste(power2$Date, power2$Time, sep = ' '), '%Y-%m-%d %H:%M:%S')
+
+## Plot
+plot(power2$Time, power2$Global_active_power, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+dev.copy(png, 'plot1.png', height=480, width=480)
+dev.off()
